e24c2de554196a0c9ecdc91b932ddd95645a3fc7
Jamie Quigley <jehq500@york.ac.uk>
2020-02-04 12:07:00 +0000

    Added task A1c, refactored task function names

--- main.c
+++ main.c
@@ -40,6 +40,7 @@ void EINT3_IRQHandler() {
     }
 }
 
+
 int main() {
     extern int8_t menu_index;
     serial_init();
@@ -48,19 +49,17 @@ int main() {
     menu_init();
     systick_init();
     sensor_enable();
-
     GPIO_IntCmd(0, 1 << 23, 1);
     NVIC_EnableIRQ(EINT3_IRQn);
     keypad_set_as_inputs();
-
     serial_printf("\r\nhello\r\n");
-
-    menu_add_option("A1a: Circle", 0, task_A1a);
-    menu_add_option("A1b: Square", 1, task_A1b);
-    menu_add_option("A2:  edge detec", 2, a2_edge_detection);
-    menu_add_option("A3:  man move", 3, a3_manual_move);
-    menu_add_option("B1:  CRGB move", 4, b1_xyz_move_rgb);
-
+    menu_add_option("Grid Home", 0, grid_home);
+    menu_add_option("A1a: Circle", 1, task_A1a_circle);
+    menu_add_option("A1b: Square", 2, task_A1b_square);
+    menu_add_option("A1c: Z axis", 3, task_A1c_z_axis);
+    menu_add_option("A2:  edge detec", 4, task_A2_edge_detection);
+    menu_add_option("A3:  man move", 5, task_A3_manual_move);
+    menu_add_option("B1:  CRGB move", 6, b1_xyz_move_rgb);
     menu_draw(0);
 
     while(1) {
@@ -73,6 +72,7 @@ int main() {
             case '#':
                 serial_printf("[Menu]: Called menu item %i\r\n", menu_index);
                 menu_run_callback(menu_index);
+                menu_draw(0);
 
             default:
                 break;

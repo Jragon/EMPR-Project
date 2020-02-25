b78ef7ea3a3fafb013653e6bdd361f1de93490f4
Rory <rory@jragon.co.uk>
2020-02-25 14:02:53 +0000

    add flag data

--- individual/rory/data.h
+++ individual/rory/data.h
@@ -1,5 +1,6 @@
 #include <lpc_types.h>
 
+#include "data_len.h"
 struct data {
     char name[16];
     uint16_t vals[16][3];
@@ -7,6 +8,7 @@ struct data {
     uint16_t height;
     // multiplied by 1000
     uint16_t error;
+    uint16_t error_rev;
 } typedef data_t;
 
 extern data_t data[];
